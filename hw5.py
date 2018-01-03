37f36c619518340d32b2ed9c6d7b986feb0c4896
Ido Kessler <Kessido@Gmail.com>
2018-01-03 18:31:27 +0200

    visual pushing

--- hw5.py
+++ hw5.py
@@ -1,4 +1,5 @@
 import matplotlib as mpl
+
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import os
@@ -36,69 +37,80 @@ train_data = sklearn.preprocessing.scale(train_data_unscaled, axis=0, with_std=F
 test_data = sklearn.preprocessing.scale(test_data_unscaled, axis=0, with_std=False)
 
 
-def test_error(C, S):
-    return 1 - sum([1 for x in S if C(x[0]) == x[1]]) / float(len(S))
+def test_error(c, s):
+    return 1 - sum([1 for x in s if c(x[0]) == x[1]]) / float(len(s))
 
 
-def sort_by_pixel(S, j, D):
-    array = zip(S,D)
-    array.sort(key=lambda x: x[0][0][j])  # sort by the j pixel
-    return [a for a,b in array], [b for a,b in array]
+def sort_by_pixel(s, j, d):
+    if not (j in cached_permutation_sorted_by):
+        S_with_indexes = zip(s, [i for i in range(len(s))])
+        S_with_indexes.sort(key=lambda x: x[0][0][j])  # sort by the j pixel
+        cached_permutation_sorted_by[j] = [index for _, index in S_with_indexes]
+    return [s[i] for i in cached_permutation_sorted_by[j]], [d[i] for i in cached_permutation_sorted_by[j]]
 
-def ERM_for_decition_stump(S, D):
-    number_of_pixels = len(S[0][0])
+
+cached_permutation_sorted_by = {}
+
+
+def erm_for_decision_stump(origin_s, origin_d):
+    number_of_pixels = len(origin_s[0][0])
     best_j = 1
     best_theta = 0
     best_F = float("inf")
-    starting__f = sum([D[i] for i in range(len(S)) if S[i][1] == 1])
-    for j in range(number_of_pixels):
-        S, D = sort_by_pixel(S, j, D)
-        current_f = starting__f
-        if best_F > current_f:
-            best_F = current_f
-            best_j = j
-            best_theta = S[0][0][j] - 1
-        for i in range(len(S)):
-            current_f -= S[i][1] * D[i]
-            if best_F > current_f and (len(S)==i+1 or S[i][0][j] != S[i + 1][0][j]):
+    best_direction = -1
+    for direction in range(2):
+        direction = direction * 2 - 1
+        starting__f = sum([origin_d[i] for i in range(len(origin_s)) if origin_s[i][1] == direction])
+        for j in range(number_of_pixels):
+            S, D = sort_by_pixel(origin_s, j, origin_d)
+            current_f = starting__f
+            if best_F > current_f:
+                best_direction = direction
                 best_F = current_f
                 best_j = j
-                if i + 1 == len(S):
-                    best_theta = (S[i][0][j]*2 + 1) / 2
-                else:
-                    best_theta = (S[i][0][j] + S[i + 1][0][j]) / 2
+                best_theta = S[0][0][j] - 1
+            for i in range(len(S)):
+                current_f -= direction * S[i][1] * D[i]
+                if best_F > current_f and (len(S) == i + 1 or S[i][0][j] != S[i + 1][0][j]):
+                    best_direction = direction
+                    best_F = current_f
+                    best_j = j
+                    if i + 1 == len(S):
+                        best_theta = (S[i][0][j] * 2 + 1) / 2
+                    else:
+                        best_theta = (S[i][0][j] + S[i + 1][0][j]) / 2
 
-    return lambda x: 1 if (x[best_j] > best_theta) else -1
+    return lambda x: best_direction if (x[best_j] > best_theta) else -best_direction
 
 
-def empirical_eroor(D, S, h):
+def empirical_error(D, S, h):
     return sum([D[i] for i in range(len(D)) if S[i][1] != h(S[i][0])])
 
 
 def ada_boost(S, T, tests):
     D = [float(1) / float(len(S)) for _ in S]
     test_errors = []
-    empirical_error = []
+    empirical_errors = []
 
     h_array = []
     w_array = []
     for t in range(T):
         print t
-        h = ERM_for_decition_stump(S, D)
-        e = empirical_eroor(D, S, h)
+        h = erm_for_decision_stump(S, D)
+        e = empirical_error(D, S, h)
         w = 0.5 * numpy.log(float(1 - e) / float(e))
         D_t = [(D[i] * numpy.exp(-w * S[i][1] * h(S[i][0]))) for i in range(len(D))]
         D = D_t / sum(D_t)
         h_array.append(h)
         w_array.append(w)
-        empirical_error.append(
+        empirical_errors.append(
             test_error(lambda x: 1 if sum([w * h(x) for w, h in zip(w_array, h_array)]) > 0 else -1, S))
         test_errors.append(
             test_error(lambda x: 1 if sum([w * h(x) for w, h in zip(w_array, h_array)]) > 0 else -1, tests))
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(empirical_error, label="empirical error")
+    ax.plot(empirical_errors, label="empirical error")
     ax.plot(test_errors, label="test error")
     plt.xlabel("iterations", fontsize=18)
     plt.ylabel("error", fontsize=16)
